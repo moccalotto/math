9cc72ab7420984b3939ad9beb41a847487658660
Kim Ravn Hansen <moccalotto@gmail.com>
2015-11-25 13:24:41 +0100

    Commentary and documentation

--- src/GeoPoint.php
+++ src/GeoPoint.php
@@ -2,11 +2,20 @@
 
 namespace Moccalotto\Math;
 
+/**
+ * GeoPoint class
+ *
+ * Represents a geolocation point on earth.
+ */
 class GeoPoint
 {
     public $lat;
     public $lon;
 
+    /**
+     * Calculate the distance between two GeoPoints
+     * @return Distance
+     */
     public static function distanceBetween(GeoPoint $p1, GeoPoint $p2)
     {
         return new Distance(69.0801317939 * sqrt(
@@ -19,22 +28,38 @@ class GeoPoint
         ), new Units\Length\Meter(), new SiPrefix('k'));
     }
 
+    /**
+     * Constructor
+     *
+     * @param int|float $lat
+     * @param int|float $lon
+     */
     public function __construct($lat, $lon)
     {
         $this->lat = (double)$lat;
         $this->lon = (double)$lon;
     }
 
+    /**
+     * @return float
+     */
     public function lat()
     {
         return $this->lat;
     }
 
+    /**
+     * @return float
+     */
     public function lon()
     {
         return $this->lon;
     }
 
+    /**
+     * @param GeoPoint $other
+     * @return Distance
+     */
     public function distanceTo(GeoPoint $other)
     {
         return static::distanceBetween($this, $other);
