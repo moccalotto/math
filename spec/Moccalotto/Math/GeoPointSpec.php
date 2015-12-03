6628e07cac223433daf151f9239e33167997420e
Kim Ravn Hansen <moccalotto@gmail.com>
2015-12-03 11:31:23 +0100

    More PHP-CS-fixes

--- spec/Moccalotto/Math/GeoPointSpec.php
+++ spec/Moccalotto/Math/GeoPointSpec.php
@@ -3,11 +3,10 @@
 namespace spec\Moccalotto\Math;
 
 use PhpSpec\ObjectBehavior;
-use Prophecy\Argument;
 
 class GeoPointSpec extends ObjectBehavior
 {
-    function it_is_initializable()
+    public function it_is_initializable()
     {
         $this->beConstructedWith(0, 0);
         $this->shouldHaveType('Moccalotto\Math\GeoPoint');
@@ -15,19 +14,19 @@ class GeoPointSpec extends ObjectBehavior
         $this->lon()->shouldReturn(0.0);
     }
 
-    function it_converts_lat_to_floats()
+    public function it_converts_lat_to_floats()
     {
         $this->beConstructedWith(0, 0);
         $this->lat()->shouldReturn(0.0);
     }
 
-    function it_converts_lon_to_floats()
+    public function it_converts_lon_to_floats()
     {
         $this->beConstructedWith(0, 0);
         $this->lon()->shouldReturn(0.0);
     }
 
-    function it_can_measure_distance_between_geo_points_with_good_precision()
+    public function it_can_measure_distance_between_geo_points_with_good_precision()
     {
         $this->beConstructedWith(0, 0);
         $distance = $this->distanceTo(new \Moccalotto\Math\GeoPoint(10, 0));
@@ -38,7 +37,7 @@ class GeoPointSpec extends ObjectBehavior
     public function getMatchers()
     {
         return [
-            'beCloseTo' => function($value, $float, $precision = 0.00001) {
+            'beCloseTo' => function ($value, $float, $precision = 0.00001) {
                 return abs($value - $float) < $precision;
             },
         ];
